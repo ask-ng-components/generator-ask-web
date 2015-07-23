6cbd2e52582be5e19db0d843df03e7c11628d5e2
Jeremy Granadillo <akakitsu@gmail.com>
2015-07-23 12:30:43 +0200

    Move gulp specific parts

--- generators/app/index.js
+++ generators/app/index.js
@@ -335,46 +335,6 @@ module.exports = generators.Base.extend({
     },
 
     /**
-     * List files extension processed by the generator
-     */
-    computeProcessedFileExtension: function () {
-      this.processedFileExtension = [
-        'html',
-        'css',
-        'js',
-        this.props.cssPreprocessor.extension,
-        this.props.jsPreprocessor.extension,
-        this.props.htmlPreprocessor.extension
-      ];
-      if (this.imageMin) {
-        this.processedFileExtension = this.processedFileExtension.concat(['jpg', 'png', 'gif', 'svg']);
-      }
-      this.processedFileExtension = _.chain(this.processedFileExtension)
-        .uniq()
-        .filter(_.isString)
-        .value()
-        .join(',');
-    },
-
-    /**
-     * Compute gulp inject task dependencies depending on js and css preprocessors
-     */
-    computeWatchTaskDeps: function () {
-
-      this.watchTaskDeps = [];
-
-      if (this.props.jsPreprocessor.srcExtension === 'es6') {
-        this.watchTaskDeps.push('\'scripts:watch\'');
-      }
-
-      if (this.props.htmlPreprocessor.key !== 'none') {
-        this.watchTaskDeps.push('\'markups\'');
-      }
-
-      this.watchTaskDeps.push('\'inject\'');
-    },
-
-    /**
      * Reject files from files.json
      * Some important files are listed in the files.json even if they are not needed
      * depending on options. This step reject these files.
@@ -482,37 +442,9 @@ module.exports = generators.Base.extend({
 
     },
 
-    computeWiredepExclusions: function () {
-      this.wiredepExclusions = [];
-      if (this.props.jQuery.key === 'none') {
-        this.wiredepExclusions.push('/jquery/');
-      }
-      if (this.props.ui.key === 'bootstrap') {
-        if(this.props.bootstrapComponents.key !== 'official') {
-          this.wiredepExclusions.push('/bootstrap\.js$/');
-          if(this.props.cssPreprocessor.extension === 'scss') {
-            this.wiredepExclusions.push('/bootstrap-sass-official\\/.*\\.js/');
-          }
-        }
-        if(this.props.cssPreprocessor.key !== 'none') {
-          this.wiredepExclusions.push('/bootstrap\\.css/');
-        }
-      } else if (this.props.ui.key === 'foundation') {
-        if(this.props.foundationComponents.key !== 'official') {
-          this.wiredepExclusions.push('/foundation\\.js/');
-        }
-        if(this.props.cssPreprocessor.extension === 'scss') {
-          this.wiredepExclusions.push('/foundation\\.css/');
-        }
-      }
-    },
-
     saveOtherProps: function () {
-      this.config.set('wiredepExclusions', this.wiredepExclusions);
       this.config.set('appName', this.appName);
       this.config.set('imageMin', this.imageMin);
-      this.config.set('watchTaskDeps', this.watchTaskDeps);
-      this.config.set('processedFileExtension', this.processedFileExtension);
     }
   }, // end configuring
 
--- generators/gulp/index.js
+++ generators/gulp/index.js
@@ -1,5 +1,6 @@
 'use strict';
 var generators = require('yeoman-generator');
+var _ = require('lodash');
 
 module.exports = generators.Base.extend({
   initializing: function () {
@@ -7,11 +8,75 @@ module.exports = generators.Base.extend({
     this.props =  this.config.get('props');
     this.appName = this.config.get('appName');
     this.imageMin = this.config.get('imageMin');
-    this.processedFileExtension = this.config.get('processedFileExtension');
-    this.watchTaskDeps = this.config.get('watchTaskDeps');
-    this.wiredepExclusions =  this.config.get('wiredepExclusions');
     this.qrCode = false;
 
+  },
+
+  configuring: {
+    /**
+     * List files extension processed by the generator
+     */
+    computeProcessedFileExtension: function () {
+      this.processedFileExtension = [
+        'html',
+        'css',
+        'js',
+        this.props.cssPreprocessor.extension,
+        this.props.jsPreprocessor.extension,
+        this.props.htmlPreprocessor.extension
+      ];
+      if (this.imageMin) {
+        this.processedFileExtension = this.processedFileExtension.concat(['jpg', 'png', 'gif', 'svg']);
+      }
+      this.processedFileExtension = _.chain(this.processedFileExtension)
+        .uniq()
+        .filter(_.isString)
+        .value()
+        .join(',');
+    },
+
+    /**
+     * Compute gulp inject task dependencies depending on js and css preprocessors
+     */
+    computeWatchTaskDeps: function () {
+
+      this.watchTaskDeps = [];
+
+      if (this.props.jsPreprocessor.srcExtension === 'es6') {
+        this.watchTaskDeps.push('\'scripts:watch\'');
+      }
+
+      if (this.props.htmlPreprocessor.key !== 'none') {
+        this.watchTaskDeps.push('\'markups\'');
+      }
+
+      this.watchTaskDeps.push('\'inject\'');
+    },
+
+    computeWiredepExclusions: function () {
+      this.wiredepExclusions = [];
+      if (this.props.jQuery.key === 'none') {
+        this.wiredepExclusions.push('/jquery/');
+      }
+      if (this.props.ui.key === 'bootstrap') {
+        if(this.props.bootstrapComponents.key !== 'official') {
+          this.wiredepExclusions.push('/bootstrap\.js$/');
+          if(this.props.cssPreprocessor.extension === 'scss') {
+            this.wiredepExclusions.push('/bootstrap-sass-official\\/.*\\.js/');
+          }
+        }
+        if(this.props.cssPreprocessor.key !== 'none') {
+          this.wiredepExclusions.push('/bootstrap\\.css/');
+        }
+      } else if (this.props.ui.key === 'foundation') {
+        if(this.props.foundationComponents.key !== 'official') {
+          this.wiredepExclusions.push('/foundation\\.js/');
+        }
+        if(this.props.cssPreprocessor.extension === 'scss') {
+          this.wiredepExclusions.push('/foundation\\.css/');
+        }
+      }
+    }
   },
 
   writing: function () {
