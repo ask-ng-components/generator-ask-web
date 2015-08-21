5beca5446d4c72f88c79636fbe6e389cdf414db0
Jeremy Granadillo <akakitsu@gmail.com>
2015-08-21 14:34:10 +0200

    add route for login

--- generators/app/templates/src/app/_ngroute/__ngroute.es6
+++ generators/app/templates/src/app/_ngroute/__ngroute.es6
@@ -1,6 +1,13 @@
 function routerConfig ($routeProvider) {
   'ngInject';
   $routeProvider
+<% if (props.projectType === 'askApp') { -%>
+    .when('/login', {
+      templateUrl: 'app/login/login.html',
+      controller: 'LoginController',
+      controllerAs: 'login'
+    })
+<% } -%>
     .when('/', {
       templateUrl: 'app/main/main.html',
       controller: 'MainController',
--- generators/app/templates/src/app/_ngroute/__ngroute.ts
+++ generators/app/templates/src/app/_ngroute/__ngroute.ts
@@ -4,6 +4,13 @@ module <%- props.appName %> {
   /** @ngInject */
   export function routerConfig($routeProvider: ng.route.IRouteProvider) {
     $routeProvider
+<% if (props.projectType === 'askApp') { -%>
+      .when('/login', {
+        templateUrl: 'app/login/login.html',
+        controller: 'LoginController',
+        controllerAs: 'login'
+      })
+<% } -%>
       .when('/', {
         templateUrl: 'app/main/main.html',
         controller: 'MainController',
